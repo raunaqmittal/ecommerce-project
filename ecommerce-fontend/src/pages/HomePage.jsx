51acfdb28592e33eeb79244d602c20b071c7ed8f
raunaqmittal <raunaqmittal2004@gmail.com>
2026-02-16 19:33:29 +0530

    used data from backend on homepage using useEffect and useState

--- ecommerce-fontend/src/pages/HomePage.jsx
+++ ecommerce-fontend/src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Header } from "../components/Header";
-import { products } from "../../Starter Files/data/products";
+
 
 import "./HomePage.css";
 
 export function HomePage() {
 
-  axios.get("http://localhost:3000/api/products")
-    .then((response)=>{
-      console.log(response.data);
-    })
+  const [products, setProductsData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products")
+      .then((response)=>{
+        console.log(response.data);
+        setProductsData(response.data);
+      })
+  }, []);
 
   return (
     <>
