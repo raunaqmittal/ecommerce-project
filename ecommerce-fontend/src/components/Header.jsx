67fe4769479bf29325d23f14e01402dd50007d2c
raunaqmittal <raunaqmittal2004@gmail.com>
2026-02-18 17:34:15 +0530

    updated site name in header

--- ecommerce-fontend/src/components/Header.jsx
+++ ecommerce-fontend/src/components/Header.jsx
@@ -12,8 +12,8 @@ export function Header({ cart }) {
     <div className="header">
       <div className="left-section">
         <Link to="/" className="header-link">
-          <img className="logo" src="images/logo-white.png" />
-          <img className="mobile-logo" src="images/mobile-logo-white.png" />
+          
+          <span style={{ fontSize: '34px', fontWeight: '700', letterSpacing: '0.5px',color:"whitesmoke" }}>E-MART</span>
         </Link>
       </div>
 
